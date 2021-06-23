85ac46f3e1c3689c67f104593c6842c4bf08b7b4
Mark Baggett <mbagget1@utk.edu>
2021-06-23 11:18:39 -0400

    Add basic generator.

---
+++ generate.py
@@ -0,0 +1,23 @@
+from data.audio import audio
+from data.books import books
+from data.large import large_images
+from data.video import video
+from change.change import Collection
+
+
+def get_all():
+    all = []
+    for item in large_images:
+        all.append(item)
+    for item in audio:
+        all.append(item)
+    for item in video:
+        all.append(item)
+    for item in books:
+        all.append(item)
+    return all
+
+
+if __name__ == "__main__":
+    x = Collection(get_all())
+    all_chunks = x.generate_pages()
