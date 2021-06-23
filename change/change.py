5a453eb6e3a04cb61c52e73b3298f75aa9dc2bff
Mark Baggett <mbagget1@utk.edu>
2021-06-23 10:52:56 -0400

    Generate pages of activities.

--- change/change.py
+++ change/change.py
@@ -6,12 +6,32 @@ class Collection:
     def __init__(self, items, items_per_page=100, base_url="https://example.com"):
         self.size = len(items)
         self.items_per_page = items_per_page
+        self.items = items
+        self.chunks = self.build_chunks()
         self.base = base_url
         self.id = f"{base_url}/activity/all-changes"
         self.last_page = self.__get_last_page()
 
     def __get_last_page(self):
         return ceil(self.size / self.items_per_page)
+
+    def build_chunks(self):
+        return [
+            self.items[i * self.items_per_page : (i + 1) * self.items_per_page]
+            for i in range(
+                (len(self.items) + self.items_per_page - 1) // self.items_per_page
+            )
+        ]
+
+    def generate_pages(self):
+        i = 1
+        is_last_page = False
+        for chunk in self.chunks:
+            if i == self.last_page:
+                is_last_page = True
+            PageOfChanges(page_number=i, pids=chunk, base_url=self.base, is_last_page=is_last_page).serialize()
+            i += 1
+        return
 
     def build(self):
         collection = {
@@ -28,7 +48,6 @@ class Collection:
                 "type": "OrderedCollectionPage",
             },
         }
-
         return collection
 
 
@@ -66,8 +85,8 @@ class PageOfChanges:
         return page_of_changes
 
     def serialize(self):
-        with open(f"activity/page-{self.page_number}", "w") as page_activity:
-            json.dump(self.build, page_activity)
+        with open(f"activity/page-{self.page_number}.json", "w") as page_activity:
+            json.dump(self.build(), page_activity, indent=4)
 
 
 class LevelZeroActivity:
